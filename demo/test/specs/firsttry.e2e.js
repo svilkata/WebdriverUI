364fc3778669b59de01967618879f50c7d49d976
svilkata <svilkata_sh@abv.bg>
2023-02-20 00:25:39 +0200

    waitUntil

--- demo/test/specs/firsttry.e2e.js
+++ demo/test/specs/firsttry.e2e.js
@@ -7,20 +7,26 @@ describe("ECommerce application", async () => {
 
         // Css selector or Xpath
         await $("#username").setValue("rahulshettyacademy");
-        await browser.pause(3000);
+        // await browser.pause(3000);
         await $("input[name='username']").setValue("secondRahul");
-        await browser.pause(3000);
+        // await browser.pause(3000);
         await $("//input[@name='username']").setValue("third Rahul");
-        await browser.pause(3000);
+        // await browser.pause(3000);
 
         const password = $("//input[@type='password']");
         await password.setValue("learning");
         await browser.pause(3000);
 
-        await $("#signInBtn").click();
-        await browser.pause(3000);
+        await $("#signInBtn").click(); //becomes signing
+        // await browser.pause(3000);
         await console.log(await $(".alert-danger").getText());
-  
-    })
+        await browser.waitUntil(async() => await $("#signInBtn").getAttribute('value') === 'Sign In', {
+            timeout: 5000,
+            timeoutMsg: 'Error message is not showing up'
+        });
+        await console.log(await $(".alert-danger").getText());
 
+        await expect($("p")).toHaveTextContaining("username is rahulshettyacademy and Password is learning");
+
+    })
 })
