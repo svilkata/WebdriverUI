b1cf3868ab87413f9a8c0c82f1de56d328d83b92
Svilen Velikov <svilen.veikov@gmail.com>
2023-02-22 18:07:36 +0200

    UI controllers and chai

--- demo/test/specs/uiControllers.e2e.js
+++ demo/test/specs/uiControllers.e2e.js
@@ -1,21 +1,35 @@
+import { expect as expectchai } from "chai";
+
 describe("UI Controls Test Suite", async () => {
 
     it("UI Controls", async () => {
         await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
-        await $("input[name='username']").setValue("rahulshettyacademy");
-        const password = $("//input[@type='password']");
-        await password.setValue("learning");
-        await $("#signInBtn").click(); //becomes signing
-
+        
         const radioButtons = await $$(".customradio");   //$$ взема всички елементи с този клас
-        const userDropdown = radioButtons[1];
-        await userDropdown.$("span").click();  //chaining - locators takes the first span present`
+        const userRadio = radioButtons[1];
+        await userRadio.$("input").click();  //chaining - locators takes the first input present`
 
-        const modal = await $(".modal-body");
+        const modal = await $(".modal-content");
         await modal.waitForDisplayed();
-        await (await $("#cancelBtn")).click();
-        console.log(await (await $$(".customradio")[0].$("span")).isSelected());
+        await modal.$("#cancelBtn").click();
+        console.log("Clicking cancel button do not change the already selected radiobutton Admin", await $$(".customradio")[0].$("input").isSelected());
 
+        await userRadio.$("input").click();
+        await modal.waitForDisplayed();
+        await $("#okayBtn").click();
 
+        //validate pop up not shown up when you select admin user
+        await $$(".customradio")[0].$("input").click();
+        await expect(modal).not.toBeDisplayed();
+
+        //dropdown select
+        const dropdown = await $("select.form-control");
+        dropdown.selectByAttribute('value', 'teach'); //селектира ни елемент от dropdown менюто
+        await dropdown.selectByVisibleText("Consultant");
+        await dropdown.selectByIndex(0);
+        console.log("currently selected option", await dropdown.getValue());
+
+        //chai assertions
+        expectchai(await dropdown.getValue()).to.equal("stud");
     })
 })
