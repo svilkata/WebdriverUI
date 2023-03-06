fc36bad4f5a01977025f7f0c41a2368fa8a4f726
svilkata <svilkata_sh@abv.bg>
2023-03-07 00:15:50 +0200

    windows and frames demo finished

--- demo/test/specs/windowsFrames.js
+++ demo/test/specs/windowsFrames.js
@@ -1,24 +1,48 @@
 describe("Windows and Frames Miscellanous", async () => {
 
-    it("Parent and Child windows switch", async () => {
-        await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
+    xit("Parent and Child windows switch", async () => {
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise/"); //window is open by automation WebdriverUI
+        await browser.pause(3000);
         await $(".blinkingText").click();
 
         const handles = await browser.getWindowHandles(); //how many windows/tabs are open at the moment
 
-        await browser.switchToWindow(handles[1]); //go the second CHILD windows/tab
+        // switchToWindow - when windows are open by application from the webpage
+        await browser.switchToWindow(handles[1]); //go the second CHILD windows/tab //window is open by application from the webpage
+        await browser.pause(3000);
         console.log(await $("h1").getText());
         console.log(await browser.getTitle()); //RS Academy
         await browser.closeWindow(); //closing the actively switched window
+        await browser.pause(3000);
 
         await browser.switchToWindow(handles[0]); //return back to PARENT window
         console.log(await browser.getTitle()); //LoginPage Practise | Rahul Shetty Academy
+        await browser.pause(3000);
 
         // go directly and open brand new window
-        await browser.newWindow("https://google.com");
+        await browser.newWindow("https://rahulshettyacademy.com"); //no need to switch here //window is open by automation WebdriverUI
         // focused on the last open window
-        console.log("---I AM HERE---", await browser.getTitle()); //Selenium, API Testing, Software Testing &amp; More QA Tutorials  | Rahul Shetty Academy
+        // works only on Chrome:
+        console.log(await browser.getTitle()); //Selenium, API Testing, Software Testing &amp; More QA Tutorials  | Rahul Shetty Academy
         
+        // switchWindow - when windows is open by automation WebdriverUI
+        await browser.switchWindow("https://rahulshettyacademy.com/loginpagePractise/");
 
+        await $("#username").setValue("helloIswitchedback");
+        await browser.pause(3000);
+    })
+
+    it("Frames switch", async () => {
+        await browser.url("https://rahulshettyacademy.com/AutomationPractice/");
+        await $("#mousehover").scrollIntoView();
+        const linksCount = await $$("a").length; //27
+        console.log(linksCount);
+
+        await browser.switchToFrame(await $("[id='courses-iframe']"));
+        console.log(await $("=Courses").getTagName());
+        console.log(await $$("a").length); //107
+
+        await browser.switchToFrame(null); //exiting the iframe and going back to normal page
+        console.log(await $$("a").length); //27
     })
 })
--- demo/wdio.conf.js
+++ demo/wdio.conf.js
@@ -58,8 +58,8 @@ export const config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        // browserName: 'chrome',
-        browserName: 'firefox',
+        browserName: 'chrome',
+        // browserName: 'firefox',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
@@ -113,8 +113,8 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: ['chromedriver'],
-    services: ['selenium-standalone'],
+    services: ['chromedriver'],
+    // services: ['selenium-standalone'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
